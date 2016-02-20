ca4e73960817f0f1be59d95ea5c6223cc13faf15
David Sudia <dsudia@gmail.com>
2016-02-20 15:59:08 -0700

    moved restaurants object to another file and exported/required to index.js

---
+++ src/server/routes/restaurants.js
@@ -0,0 +1,46 @@
+var restaurants = {
+  1: {
+    name: 'Los Tacos',
+    cuisine: 'Mexican Cuisine',
+    loc: 'Denver, CO',
+    rating: 5,
+    image: '/images/mexican.png'
+  },
+  2: {
+    name: 'Burger Bar',
+    cuisine: 'American Cuisine',
+    loc: 'Seattle, WA',
+    rating: 5,
+    image: '/images/burger.png'
+  },
+  3: {
+    name: 'Pasta Freddy\'s',
+    cuisine: 'Italian Cuisine',
+    loc: 'Sacramento, CA',
+    rating: 3,
+    image: '/images/italian.png'
+  },
+  4: {
+    name: 'Bangkok Grill',
+    cuisine: 'Thai Cuisine',
+    loc: 'Brooklyn, NY',
+    rating: 2,
+    image: '/images/thai.jpg'
+  },
+  5: {
+    name: 'Pho Mazing',
+    cuisine: 'Vietnamese Cuisine',
+    loc: 'Boulder, CO',
+    rating: 2,
+    image: '/images/pho.jpg'
+  },
+  6: {
+    name: 'Fiestaritos',
+    cuisine: 'Mexican Cuisine',
+    loc: 'Lincoln, NE',
+    rating: 1,
+    image: '/images/mexican.png'
+  }
+};
+
+module.exports = restaurants;
