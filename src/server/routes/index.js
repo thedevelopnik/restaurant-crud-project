6d569841e8dd1e0166681322a405037a5ec962d7
David Sudia <dsudia@gmail.com>
2016-02-20 15:52:35 -0700

    populate restaurants object with restaurant name, cuisine and loc

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -3,22 +3,34 @@ var router = express.Router();
 
 var restaurants = {
   1: {
-
+    name: 'Los Tacos',
+    cuisine: 'Mexican Cuisine',
+    loc: 'Denver, CO'
   },
   2: {
-
+    name: 'Burger Bar',
+    cuisine: 'American Cuisine',
+    loc: 'Seattle, WA'
   },
   3: {
-
+    name: 'Pasta Freddy\'s',
+    cuisine: 'Italian Cuisine',
+    loc: 'Sacramento, CA'
   },
   4: {
-
+    name: 'Bangkok Grill',
+    cuisine: 'Thai Cuisine',
+    loc: 'Brooklyn, NY'
   },
   5: {
-
+    name: 'Pho Mazing',
+    cuisine: 'Vietnamese Cuisine',
+    loc: 'Boulder, CO'
   },
   6: {
-
+    name: 'Fiestaritos',
+    cuisine: 'Mexican Cuisine',
+    loc: 'Lincoln, NE'
   }
 };
 
