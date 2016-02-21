fcbd00200834a2c131feec512b17585b43f98574
David Sudia <dsudia@gmail.com>
2016-02-20 17:49:11 -0700

    draft html on index.html

--- src/server/routes/pages.js
+++ src/server/routes/pages.js
@@ -2,7 +2,10 @@ var restaurants = require('restaurants');
 
 var pages = {
   '/': {
-
+    title: 'gTables',
+    name: 'gTables',
+    link: '/restaurants/new',
+    linkText: 'New Restaurant'
   },
 };
 
