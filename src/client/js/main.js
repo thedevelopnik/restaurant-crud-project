4ad1667c67e07b15f75a9e90db3ce226a813bd4d
David Sudia <dsudia@gmail.com>
2016-02-27 19:41:23 -0700

    review truncation and expansion work correctly

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,6 +1,9 @@
 // add scripts
+var date = document.getElementById('date-picker');
+if (date) {
+  date.valueAsDate = new Date();
+}
 
-document.getElementById('date-picker').valueAsDate = new Date();
 
 function delRes () {
   var thisUrl = window.location.href;
@@ -23,4 +26,22 @@ $('#rateit1').on('click', function() {
   var ratingVal = $('#rateit1').rateit('value');
   console.log(ratingVal);
   $('#backing2').attr('value', ratingVal);
+});
+
+var truncated = $('.truncate');
+console.log(truncated);
+var truncLength = 35;
+
+truncated.each(function() {
+  var text = $(this).html();
+  if (text.length > truncLength) {
+    $(this).html(text.slice(0, truncLength) + '<a href="#" class="expand"><span>...</span></a>' +
+  '<span style="display: none;">' + text.slice(truncLength, text.length));
+  }
+});
+
+$('.expand').on('click', function() {
+  event.preventDefault();
+  $(this).hide();
+  $(this).next().show();
 });
