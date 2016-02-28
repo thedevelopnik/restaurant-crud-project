428eded07dd022e2d013c74840b211a472634744
David Sudia <dsudia@gmail.com>
2016-02-27 17:25:18 -0700

    new review route created

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -154,4 +154,29 @@ router.post('/restaurants/:id/edit', function(req, res, next) {
   });
 });
 
+router.get('/restaurants/:id/reviews/new', function(req, res, next) {
+  var id = req.params.id;
+  var responseArray = [];
+  pg.connect(connectionString, function(err, client, done) {
+
+    if(err) {
+      console.log(err);
+      done();
+      return res.status(500).json({status: 'error',message: 'Something didn\'t work'});
+    }
+
+    var query = client.query('select * from restaurants where id=' + id);
+    query.on('row', function(row) {
+      responseArray.push(row);
+    });
+
+    query.on('end', function() {
+      console.log(responseArray);
+      res.render('reviews/new', {restaurants: responseArray[0]});
+      done();
+    });
+     pg.end();
+  });
+});
+
 module.exports = router;
