114e9cd8714cac17aadc529457c09235acbed484
David Sudia <dsudia@gmail.com>
2016-03-01 15:48:11 -0700

    show page refactored to use knex

--- src/server/routes/restaurants/show.js
+++ src/server/routes/restaurants/show.js
@@ -1,33 +1,49 @@
 module.exports = function (req, res, next, knex) {
   var id = req.params.id;
-  var resArray = [];
-  var reviewArray = [];
+  var restaurantInfo;
+  var reviews;
 
-  pg.connect(db, function(err, client, done) {
-
-    if(err) {
+  knex('restaurants').select().where('id', id)
+    .catch(function(err) {
       console.log(err);
-      done();
-      return res.status(500).json({status: 'error',message: 'Something didn\'t work'});
-    }
-
-    var queryResInfo = client.query('select * from restaurants where id=' + id);
-    queryResInfo.on('row', function(row) {
-      resArray.push(row);
-    });
-    queryResInfo.on('end', function() {
-      done();
+    })
+    .then(function(data) {
+      restaurantInfo = data;
     });
 
-    var queryRevs = client.query('select * from reviews where res_id=' + id);
-    queryRevs.on('row', function(row) {
-      reviewArray.push(row);
+  knex('reviews').select().where('res_id', id)
+    .catch(function(err) {
+      console.log(err);
+    }).then(function(data) {
+      reviews = data;
+    }).then(function(data) {
+      res.render('restaurants/show', {restaurants: restaurantInfo[0], reviews: reviews});
     });
-
-    queryRevs.on('end', function() {
-      res.render('restaurants/show', {restaurants: resArray[0], reviews: reviewArray});
-      done();
-    });
-     pg.end();
-  });
+  // pg.connect(db, function(err, client, done) {
+  //
+  //   if(err) {
+  //     console.log(err);
+  //     done();
+  //     return res.status(500).json({status: 'error',message: 'Something didn\'t work'});
+  //   }
+  //
+  //   var queryResInfo = client.query('select * from restaurants where id=' + id);
+  //   queryResInfo.on('row', function(row) {
+  //     resArray.push(row);
+  //   });
+  //   queryResInfo.on('end', function() {
+  //     done();
+  //   });
+  //
+  //   var queryRevs = client.query('select * from reviews where res_id=' + id);
+  //   queryRevs.on('row', function(row) {
+  //     reviewArray.push(row);
+  //   });
+  //
+  //   queryRevs.on('end', function() {
+  //     res.render('restaurants/show', {restaurants: resArray[0], reviews: reviewArray});
+  //     done();
+  //   });
+  //    pg.end();
+  // });
 };
