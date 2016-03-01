b2e9ad689e6bdefe639cbd2c662eed765687c32f
David Sudia <dsudia@gmail.com>
2016-03-01 15:59:13 -0700

    edit page refactored to knex

--- src/server/routes/restaurants/edit.js
+++ src/server/routes/restaurants/edit.js
@@ -1,24 +1,10 @@
 module.exports = function (req, res, next, knex) {
   var id = req.params.id;
-  var responseArray = [];
-  pg.connect(db, function(err, client, done) {
-
-    if(err) {
-      console.log(err);
-      done();
-      return res.status(500).json({status: 'error',message: 'Something didn\'t work'});
-    }
-
-    var query = client.query('select * from restaurants where id=' + id);
-    query.on('row', function(row) {
-      responseArray.push(row);
+  var resInfo;
+  knex('restaurants').where('id', id)
+    .then(function(data) {
+      resInfo = data[0];
+    }).then(function(data) {
+      res.render('restaurants/edit', {restaurants: resInfo});
     });
-
-    query.on('end', function() {
-      console.log(responseArray);
-      res.render('restaurants/edit', {restaurants: responseArray[0]});
-      done();
-    });
-     pg.end();
-  });
 };
