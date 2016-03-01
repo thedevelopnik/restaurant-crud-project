3a6ffc27256c31ebcd509c3c91889cbd0fc73b5b
David Sudia <dsudia@gmail.com>
2016-03-01 16:16:47 -0700

    render review edit refactored to knex

--- src/server/routes/reviews/edit.js
+++ src/server/routes/reviews/edit.js
@@ -1,22 +1,13 @@
 module.exports = function (req, res, next, knex) {
-  var reviewArray = [];
+  var reviewInfo;
   var resId = req.params.id;
   var reviewId = req.params.reviewid;
 
-  pg.connect(db, function(err, client, done) {
-    if(err) {
-      done();
-      return res.status(500).json({status: 'error',message: 'Something didn\'t work'});
-    }
-    var queryRevs = client.query('select * from reviews where res_id=' + resId + 'and id=' + reviewId);
-    queryRevs.on('row', function(row) {
-      reviewArray.push(row);
-    });
-
-    queryRevs.on('end', function() {
-      res.render('reviews/edit', {review: reviewArray[0]});
-    });
-
-    pg.end();
+  knex('reviews').where({
+    res_id: resId,
+    id: reviewId
+  }).then(function(data) {
+    reviewInfo = data[0];
+    res.render('reviews/edit', {review: reviewInfo});
   });
 };
