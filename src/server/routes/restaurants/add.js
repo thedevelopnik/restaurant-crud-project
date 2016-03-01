e7e324331c6268b4f21f1371c8bbd673ad87b00e
David Sudia <dsudia@gmail.com>
2016-03-01 15:42:34 -0700

    add restaurant refactored to knex

--- src/server/routes/restaurants/add.js
+++ src/server/routes/restaurants/add.js
@@ -1,19 +1,21 @@
 module.exports = function (req, res, next, knex) {
   var newRes = req.body;
-  pg.connect(db, function(err, client, done) {
-    if(err) {
-      done();
-      return res.status(500).json({status: 'error',message: 'Something didn\'t work'});
-    }
-    var responseArray = [];
-    var queryGET = client.query("select id from restaurants");
-    queryGET.on('row', function(row) {
-      responseArray.push(row);
-    });
-    var queryPOST = client.query("insert into restaurants (name, city, state, cuisine, image, descrip) values ('" + newRes.name + "', '" + newRes.city + "', '" + newRes.state + "', '" + newRes.cuisine + "', '" + newRes.image + "', '" + newRes.descrip + "')");
-    queryGET.on('end', function() {
-      res.redirect('/restaurants/' + (responseArray.length + 1));
-    });
-    pg.end();
+
+  knex('restaurants').insert({
+    name: newRes.name,
+    city: newRes.city,
+    state: newRes.state,
+    cuisine: newRes.cuisine,
+    image: newRes.image,
+    descrip: newRes.descrip
+  }).catch(function(err) {
+    console.log(err);
+  })
+  .then(function(data) {
+    return knex('restaurants').select('id').where('name', newRes.name);
+  }).then(function(data) {
+    console.log(data);
+    var newId = data[0].id;
+    res.redirect('/restaurants/' + newId);
   });
 };
