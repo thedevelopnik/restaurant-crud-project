c52e7a41251865068c4b66c3833a03994712a696
David Sudia <dsudia@gmail.com>
2016-03-06 20:24:26 -0700

    add review queries broken out

---
+++ src/server/queries/reviewQueries.js
@@ -0,0 +1,18 @@
+module.exports = {
+  insertRev: function(id, newRev, ratingInt) {
+    knex('reviews').insert({
+      res_id: id,
+      rev_name: newRev.revName,
+      rev_date: newRev.revDate,
+      rating: ratingInt,
+      review: newRev.review,
+    });
+  },
+  getRating: function(id) {
+    knex('reviews').select('rating').where('res_id', id);
+  },
+  upRating: function(id, data) {
+    knex('restaurants').where('id', id)
+    .update('rating', data);
+  }
+};
--- src/server/routes/reviews/add.js
+++ src/server/routes/reviews/add.js
@@ -6,25 +6,18 @@ module.exports = function (req, res, next, knex, cb) {
   var ratingInt = Number(newRev.rating);
   var rating;
 
-  knex('reviews').insert({
-    res_id: id,
-    rev_name: newRev.revName,
-    rev_date: newRev.revDate,
-    rating: ratingInt,
-    review: newRev.review,
-  }).then(function() {
-    knex('reviews').select('rating').where('res_id', id)
-    .then(function(data) {
-      console.log('The ratings are ' + data[0].rating + data[1].rating);
-      return cb(data);
-    }).then(function(data) {
-      knex('restaurants').where('id', id)
-      .update('rating', data)
-      .catch(function(err) {
-      console.log(err);
+  queries.insertRev(id, newRev, ratingInt)
+    .then(function() {
+        queries.getRating(id)
+      .then(function(data) {
+        return cb(data);
       }).then(function(data) {
-        res.redirect('/restaurants/' + id);
+        queries.upRating(id, data)
+        .catch(function(err) {
+        console.log(err);
+        }).then(function(data) {
+          res.redirect('/restaurants/' + id);
+        });
       });
     });
-  });
 };
