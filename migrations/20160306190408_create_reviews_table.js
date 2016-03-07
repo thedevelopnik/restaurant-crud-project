0bccbddf7ee8debe426870ebd2b43aaf34698816
David Sudia <dsudia@gmail.com>
2016-03-06 19:10:36 -0700

    add instructions to migration files

--- migrations/20160306190408_create_reviews_table.js
+++ migrations/20160306190408_create_reviews_table.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('reviews', function(table) {
+    table.increments();
+    table.integer('res_id').references('restaurants.id');
+    table.string('rev_name');
+    table.date('rev_date');
+    table.decimal('rating', 3, 1);
+    table.string('review', 1000);
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('reviews');
 };
