88a42255ed64fb6ec7b05d81e711c3a954a4b4c8
David Sudia <dsudia@gmail.com>
2016-03-06 19:07:54 -0700

    create migration and seed files

---
+++ knexfile.js
@@ -0,0 +1,11 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost:5432/gTables'
+  },
+
+  production: {
+    client: 'pg',
+    connection: 'postgres://nibqwnxjuzmgcq:l8Fg8svajzaJ2VW_oXQAiEC2v1@ec2-107-21-229-87.compute-1.amazonaws.com:5432/derbheuj1neeo1?ssl=true'
+  }
+};
---
+++ migrations/20160306190352_create_restaurants_table.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('restaurants', function(table) {
+    table.increments();
+    table.string('name');
+    table.string('cuisine');
+    table.string('city');
+    table.string('state', 2);
+    table.decimal('rating', 3, 1);
+    table.string('image');
+    table.string('descrip', 1000);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('restaurants');
+};
