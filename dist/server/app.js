add0369397c29aba979cf685364fc8642a4cd1f4
David Sudia <dsudia@gmail.com>
2016-03-27 20:20:16 -0600

    and to dist app.js

--- dist/server/app.js
+++ dist/server/app.js
@@ -7,6 +7,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var swig = require('swig');
 var pg = require('pg');
+if ( !process.env.NODE_ENV ) { require('dotenv').config(); }
 
 
 // *** routes *** //
